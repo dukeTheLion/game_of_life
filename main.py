70898f3cf6602b34ad08966c146df48d95b38cec
dukeTheLion <savio_gm@icloud.com>
2020-10-16 10:47:17 -0300

    Cell

--- main.py
+++ main.py
@@ -21,5 +21,31 @@ Informacoes adicionais:
 """
 
 
+class Cell:
+    def __init__(self, pos_y, pos_x, alive):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.alive = alive
+
+    def eye(self):
+        pos = []
+
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                pos.append([self.pos_y + i, self.pos_x + j])
+
+        pos.pop(4)
+
+        # Ordem da lista
+        # 1 - 2 - 3
+        # 4 -   - 5
+        # 6 - 7 - 8
+        return pos
+
+    def __str__(self):
+        return f'██({self.pos_y}, {self.pos_x})' if self.alive else f'░░({self.pos_y}, {self.pos_x})'
+
+
 if __name__ == '__main__':
-    print('Hello world')
+    new = Cell(5, 3, True)
+    print(new.eye())
